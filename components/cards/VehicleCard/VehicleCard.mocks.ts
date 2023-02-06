479b87ba65c46e00d6f3030f9fb24639e8702159
jake-mallin <jam@clickdealer.co.uk>
2023-02-06 14:56:57 +0000

    feat: create VehicleCard

---
+++ components/cards/VehicleCard/VehicleCard.mocks.ts
@@ -0,0 +1,13 @@
+import { IVehicleCard } from './VehicleCard';
+
+const base: IVehicleCard = {
+  tag: 'Vehicles',
+  title: `Williams Car Reveal`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Jake',
+  time: '2h ago',
+};
+
+export const mockVehicleCardProps = {
+  base,
+};
